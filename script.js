01c388ece760ea63ab02e28b2deecd4825f0073e
Artemy <115584804+molodayamama@users.noreply.github.com>
2024-03-15 05:11:39 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const toggleViewBtn = document.getElementById('toggleViewBtn');
+    const imageList = document.getElementById('imageList');
 
+    // Set the default view to "list"
+    imageList.classList.add('list-view');
+
+    toggleViewBtn.addEventListener('click', function() {
+        if (imageList.classList.contains('tile-view')) {
+            imageList.classList.remove('tile-view');
+            imageList.classList.add('list-view');
+        } else {
+            imageList.classList.remove('list-view');
+            imageList.classList.add('tile-view');
+        }
+    });
+});
