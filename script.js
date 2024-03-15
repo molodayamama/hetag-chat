bb8e8111f76340fa69be7844889e496643fb218d
molodayamama <115584804+molodayamama@users.noreply.github.com>
2024-03-15 15:40:09 +0500

    13 new pics

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     const toggleViewBtn = document.getElementById('toggleViewBtn');
     const imageList = document.getElementById('imageList');
-
     // Set the default view to "list"
     imageList.classList.add('list-view');
-
-    toggleViewBtn.addEventListener('click', function() {
+    toggleViewBtn.addEventListener('click', function () {
         if (imageList.classList.contains('tile-view')) {
             imageList.classList.remove('tile-view');
             imageList.classList.add('list-view');
@@ -14,4 +12,15 @@ document.addEventListener('DOMContentLoaded', function() {
             imageList.classList.add('tile-view');
         }
     });
+    let upButton = document.querySelector('.up-button');
+    window.onscroll = function () {
+        if (window.scrollY > 200) {
+            upButton.classList.add('shown');
+        } else {
+            upButton.classList.remove('shown');
+        }
+    };
+    upButton.onclick = function () {
+        window.scrollTo(0, 0);
+    };
 });
